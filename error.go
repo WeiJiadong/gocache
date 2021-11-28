bd9086cb119fb4aca7805f51da864553c596ac91
vividwei <vividwei@tencent.com>
2021-11-28 18:00:48 +0800

    fmt error.go

--- error.go
+++ error.go
@@ -3,6 +3,6 @@ package gocache
 import "fmt"
 
 var (
-	KeyNotFoundErr = fmt.Errorf("key not found")
+	KeyNotFoundErr  = fmt.Errorf("key not found")
 	KeyIsExpiredErr = fmt.Errorf("key is expired")
 )
